08be113c047756f672fcfe66548c27f54735c55c
om-mane-coder <om.mane.tech@gmail.com>
2026-07-03 23:48:27 +0530

    Time: 0 ms (100%), Space: 8.1 MB (25.96%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+
+        int x = start ^ goal;
+        int cnt = 0;
+
+        while(x)
+        {
+            cnt += x & 1;
+            x >>= 1;
+        }
+
+        return cnt;
+        
+    }
+};
