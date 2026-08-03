7c9e82fc90743c8e9a776d76dca4991ed83372db
om-mane-coder <om.mane.tech@gmail.com>
2026-08-03 13:23:39 +0530

    Time: 0 ms (100%), Space: 8.8 MB (93.09%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int count = 0;
+
+        for(int i = s.length() - 1; i >= 0; i--)
+        {
+            if(s[i] == ' ')
+            {
+                if(count > 0)
+                   return count;
+            }
+            else
+            {
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+};
