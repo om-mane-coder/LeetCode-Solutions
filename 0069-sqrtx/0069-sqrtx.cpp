5ede708fd2347eaa506fdaba189a745f0a6c5896
om-mane-coder <om.mane.tech@gmail.com>
2026-08-06 18:45:21 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.19%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low = 1;
+        int high = x;
+        int ans = 1;
+
+       
+
+        if(x < 2)
+        {
+            return x;
+        }
+
+        while(low <= high)
+        {
+            
+        int mid = low + (high - low)/2;
+          
+          if(mid == x/mid)
+          {
+            return mid;
+            
+          }
+          else if(mid < x/mid)
+          {
+            ans = mid;
+            low = mid + 1;
+          }
+          else
+          {
+            high = mid -1;
+          }
+
+        }
+        return ans;
+        
+    }
+};
